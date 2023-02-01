0dbce97f2ef476490ce5c196142630c8fb18a07e
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-02-01 17:51:10 +0200

    feat: add js for price section

--- plants/js/script.js
+++ plants/js/script.js
@@ -1,13 +1,12 @@
 "use strict"
-console.log(`самостоятельная оценка 100\nВёрстка валидная +10\nВёрстка семантическая +20\nВёрстка соответствует макету +48\nТребования к css + 12\nИнтерактивность, реализуемая через css +20`)
-
+//Burger
 const mobileNav = document.querySelector(".mobile-nav");
 const burger = document.querySelector(".burger");
 const menu = document.querySelector(".header-nav");
 const body = document.querySelector("body");
 const overBody = document.querySelector(".overlay");
 
-const openBurger =  function () {
+const openBurger = function () {
     mobileNav.classList.toggle("_active");
     burger.classList.toggle("_active");
     body.classList.toggle('overlay');
@@ -21,4 +20,20 @@ const closeBurger = function () {
 
 burger.addEventListener("click", openBurger);
 menu.addEventListener("click", closeBurger);
-mobileNav.addEventListener ("click", closeBurger);
+mobileNav.addEventListener("click", closeBurger);
+
+//Order Buttom Prices Section
+const priceMod = document.querySelectorAll('.price-options__items');
+
+const checkOpen = function (event) {
+    if (!event.target.open) return; 
+        let priceCard = event.target.parentNode.children;
+        console.log(priceCard);
+        for (let i = 0; i < priceCard.length; i++) {
+            if (priceCard[i].tagName != "DETAILS" || !priceCard[i].hasAttribute('open') || event.target == priceCard[i]) continue;
+            priceCard[i].removeAttribute('open');
+        }
+}
+for (let i = 0; i <priceMod.length; i++ ){
+    priceMod[i].addEventListener('toggle', checkOpen)
+}
