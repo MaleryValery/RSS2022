a1a68c05e3d1234a43aa692525252e44cee3f104
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-02-13 22:09:44 +0200

    feat: add greeting and user name

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -2,7 +2,36 @@
 
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
+const greeting = document.querySelector(".greeting");
+const inputName = document.querySelector('.name');
+/***********WEELCOME**********/
+const showGreeting = function () {
+    greeting.textContent = `Good ${getTimeOfDay()}`
 
+}
+const getTimeOfDay = function () {
+    const date = new Date();
+    const hours = date.getHours();
+    if (hours >= 0 && hours < 6) return 'Night';
+    else
+    if (hours >= 6 && hours < 12) return 'Morning';
+    else
+    if (hours >= 12 && hours < 18) return 'Afternoon';
+    else
+    if (hours >= 18 && hours < 24) return 'Evening';
+}
+
+function setLocalStorage() {
+    localStorage.setItem('inputName', inputName.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    if (localStorage.getItem('inputName')) {
+        inputName.value = localStorage.getItem('inputName');
+    }
+}
+window.addEventListener('load', getLocalStorage)
 /***********DATE**********/
 const showDate = function () {
     const options = {
@@ -21,12 +50,6 @@ const showTime = function () {
     time.textContent = currentTime;
     setTimeout(showTime, 1000);
     showDate();
+    showGreeting();
 }
 showTime();
-
-/***********WEELCOME**********/
-
-
-
-
-
