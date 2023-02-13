ff072ce13b2b9a2c1a036ca8ff5c3f92293c6070
Valeriia Melnikov <valery.medvedeva@gmail.com>
2023-02-13 21:32:52 +0200

    feat: add date in momentum task

--- momentum/js/script.js
+++ momentum/js/script.js
@@ -1,18 +1,32 @@
 'use strict'
-/***********TIME**********/
+
 const time = document.querySelector('.time');
 const date = document.querySelector('.date');
 
+/***********DATE**********/
+const showDate = function () {
+    const options = {
+        weekday: "long",
+        month: 'long',
+        day: 'numeric',
+        year: "numeric",
+    };
+    const dateCurrent = new Date().toLocaleDateString('en-IL', options)
+    date.textContent = dateCurrent;
+}
+/***********TIME**********/
 const showTime = function () {
     const dateTime = new Date();
     const currentTime = dateTime.toLocaleTimeString();
     time.textContent = currentTime;
     setTimeout(showTime, 1000);
+    showDate();
 }
 showTime();
 
-/***********DATE**********/
+/***********WEELCOME**********/
 
-const showDate = function(){
-    
-}
+
+
+
+
